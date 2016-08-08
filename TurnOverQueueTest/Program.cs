fe8de01ce613fc20bba63e2cebdef9cc34c64263
July541 <zhulei1995@126.com>
2016-08-09 00:22:56 +0800

    First Demo

---
+++ TurnOverQueueTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Turnover_Queue;
+using System.Threading;
+
+namespace TurnOverQueueTest
+{
+    class Program
+    {
+        static TurnOverQueue<int> q = new TurnOverQueue<int>();
+        private static int ProID;
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(Product));
+            Thread t2 = new Thread(new ThreadStart(Consumer));
+            t2.Start();
+            t1.Start();
+            Console.ReadLine();
+        }
+        public static void Product()
+        {
+            for(int i = 0; i < 30; i++)
+            {
+                Console.WriteLine("product No." + (i + 1) + " has been producted");
+                q.Add(i + 1);
+            }
+        }
+        public static void Consumer()
+        {
+            while(true)
+            {
+                if(q.Get(out ProID))
+                {
+                    Console.WriteLine("Product No." + ProID + " has been consumed");
+                    if (ProID == 30)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("has no product yet.");
+                }
+            }
+        }
+    }
+}
---
+++ Turnover Queue/TurnOverQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Turnover_Queue
+{
+    public class TurnOverQueue<T>
+    {
+        protected Queue<T> m_frontQueue;
+        protected Queue<T> m_backQueue;
+        private static object _lock = new object();
+        public int Add(T t)
+        {
+             lock(_lock)
+             {
+                 m_frontQueue.Enqueue(t);
+                 return m_frontQueue.Count();
+             }
+        }
+        private void Swap()
+        {
+            lock(_lock)
+            {
+                Queue<T> temp = new Queue<T>();
+                temp = m_backQueue;
+                m_backQueue = m_frontQueue;
+                m_frontQueue = temp;
+            }
+        }
+        public bool Get(out T t)
+        {
+            if(m_backQueue.Count() == 0)
+            {
+                Swap();
+            }
+            if(m_backQueue.Count() == 0)
+            {
+                t = default(T);
+                return false;
+            }
+            t = m_backQueue.Dequeue();
+            return true;
+        }
+        public TurnOverQueue()
+        {
+            m_frontQueue = new Queue<T>();
+            m_backQueue = new Queue<T>();
+        }
+    }
+}
